1e91f1572e01653d6e118899d349470b93ae31e1
MDAlviano <mdalviano.21122@gmail.com>
2024-11-01 17:37:37 +0700

    membuat delete product api

--- src/main/kotlin/alviano/kotlin/restful/controller/ProductController.kt
+++ src/main/kotlin/alviano/kotlin/restful/controller/ProductController.kt
@@ -5,6 +5,7 @@ import alviano.kotlin.restful.model.ProductResponse
 import alviano.kotlin.restful.model.UpdateProductRequest
 import alviano.kotlin.restful.model.WebResponse
 import alviano.kotlin.restful.service.ProductService
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -56,6 +57,19 @@ class ProductController(val productService: ProductService) {
             code = 200,
             status = "OK",
             data = productResponse
+        )
+    }
+
+    @DeleteMapping(
+        value = ["/api/products/{idProduct}"],
+        produces = ["application/json"]
+    )
+    fun deleteProduct(@PathVariable("idProduct") id: String): WebResponse<String> {
+        productService.delete(id)
+        return WebResponse(
+            code = 200,
+            status = "OK",
+            data = "Data dengan id: $id berhasil dihapus"
         )
     }
 
